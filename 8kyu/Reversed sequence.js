4222e2281375b875bc9b1c1a83fe4fa397031663
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-06 20:21:55 +0300

    :feat: add task Reversed sequence

---
+++ 8kyu/Reversed sequence.js
@@ -0,0 +1,17 @@
+/*Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+//solid
+
+/*
+const reverseSeq = (n) => {
+ const result = [];
+  for (let i = n; i > 0; i--) {
+	result.push(i);
+  }
+  return result;
+};
+
+*/
