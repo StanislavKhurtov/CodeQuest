da67c1fb23f072e57461843bcb502f3d0458ea06
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-25 21:10:28 +0300

    refactor: change structure file

--- 8kyu/Abbreviate a Two Word Name.js
+++ 8kyu/Abbreviate a Two Word Name.js
@@ -1,23 +1,5 @@
-/*
 
-
-Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-
-The output should be two capital letters with a dot separating them.
-
-It should look like this:
-
-Sam Harris => S.H
-
-patrick feeney => P.F
-
-*/
-
-//Solid
-
-/*
-function abbrevName(name){
-  var names = name.split(" ");
-  return names[0].charAt(0).toUpperCase() + "." + names[1].charAt(0).toUpperCase();
+function abbrevName(name) {
+	var names = name.split(" ");
+	return names[0].charAt(0).toUpperCase() + "." + names[1].charAt(0).toUpperCase();
 }
-*/
--- 8kyu/Beginner - Reduce but Grow.js
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -1,17 +1,4 @@
-/*
-Beginner - Reduce but Grow
-*/
-
-/*
-Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
-
-[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
-
-*/
-
-
-/*
-function grow(x){
-return x.reduce((acc,el)=> acc*el)
+function grow(x) {
+	return x.reduce((acc, el) => acc * el)
 }
-*/
+
