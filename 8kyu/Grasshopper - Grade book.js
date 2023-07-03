ebdc599c5a652eefdd0a9875935297937873a8f9
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-07-03 20:56:35 +0300

    feat: add solution Grasshopper - Grade book

---
+++ 8kyu/Grasshopper - Grade book.js
@@ -0,0 +1,17 @@
+/*
+
+function getGrade (s1, s2, s3) {
+  let meanScore = (s1 + s2 + s3)/3;
+  if( meanScore >= 90 && meanScore <= 100){
+	return 'A';
+  } else if( meanScore >= 80 && meanScore < 90) {
+	return 'B'
+   } else if( meanScore >= 70 && meanScore < 80) {
+	return 'C'
+   } else if( meanScore >= 60 && meanScore < 70) {
+	return 'D'
+   } else {
+	return 'F'
+  }
+}
+*/
