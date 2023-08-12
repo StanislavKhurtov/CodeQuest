5f81c53416e5feaa11ac86bc4da78aef51f49c86
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-08-12 20:38:00 +0300

    feat:add solution Sum Mixed Array

---
+++ 8kyu/Sum Mixed Array.js
@@ -0,0 +1,17 @@
+/*function sumMix(x){
+    let sum = 0;
+    for ( let i = 0; i < x.length; i++) {
+        sum += + x[i]
+    }
+    return sum
+}*/
+
+
+
+// or
+
+
+/*
+function sumMix(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+}*/
