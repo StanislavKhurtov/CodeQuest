6b240a667f54e1e35ecb03f3f92b7f028cf9f144
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-07 23:02:33 +0300

    feat:add solution get character from ASCII Value

---
+++ 8kyu/get character from ASCII Value.js
@@ -0,0 +1,7 @@
+function getChar(number) {
+    if (Number.isInteger(number) && number >= 0 && number <= 127) {
+        return String.fromCharCode(number);
+    } else {
+        return null;
+    }
+}
