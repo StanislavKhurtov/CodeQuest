1571512facfa23ae102f68cd96e4b2f897d0c836
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-09-30 19:22:25 +0300

    feat:add solution Name Shuffler

--- 8kyu/Name Shuffler.js
+++ 8kyu/Name Shuffler.js
@@ -1,19 +1,3 @@
-function nameShuffler(str){
-    // Разделяем полное имя на имя и фамилию
-    const names = str.split(' ');
-    if (names.length < 2) {
-        return str; // Возвращаем полное имя без изменений, если нет пробела
-    }
-
-    const firstName = names[0];
-    const lastName = names[names.length - 1];
-
-    // Меняем местами имя и фамилию
-    names[0] = lastName;
-    names[names.length - 1] = firstName;
-
-    // Объединяем имена обратно в одну строку
-    const swappedName = names.join(' ');
-
-    return swappedName;
+function nameSuffle(str){
+    return str.split(' ').reverse().join(' ')
 }
