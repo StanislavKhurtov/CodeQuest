29d0e5816567e0b76febc91baacec7d6c50c6730
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-10-12 13:18:11 +0300

    feat:add solution Anagram Detection

---
+++ 7kyu/Anagram Detection.js
@@ -0,0 +1,6 @@
+function isAnagram(word1, word2) {
+    const sortedWord1 = word1.toLowerCase().split('').sort().join('');
+    const sortedWord2 = word2.toLowerCase().split('').sort().join('');
+
+    return sortedWord1 === sortedWord2;
+}
