b40deae26cc8d5a35a9aae5536771e46a23c5008
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-10-27 23:51:01 +0300

    feat: add solution task Shortest Word

--- 7kyu/Categorize New Member.js
+++ 7kyu/Categorize New Member.js
@@ -1,9 +1,3 @@
-function openOrSenior(members) {
-	return members.map(([age, handicap]) => {
-		if (age >= 55 && handicap > 7) {
-			return "Senior";
-		} else {
-			return "Open";
-		}
-	});
-}
+function openOrSenior(data){
+	return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
