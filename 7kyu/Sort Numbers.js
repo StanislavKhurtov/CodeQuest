dace09933ebdda6ded99832c6bc124d34248ce80
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-12-06 20:59:48 +0300

    feat: add task Sort Numbers

--- 7kyu/Sort Numbers.js
+++ 7kyu/Sort Numbers.js
@@ -3,3 +3,12 @@ function solution(nums) {
 		return a - b
 	});
 }
+
+
+function solution(nums) {
+	if (nums === null || nums.length === 0) {
+		return [];
+	}
+
+	return nums.sort((a, b) => a - b);
+}
