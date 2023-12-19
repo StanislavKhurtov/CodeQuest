94f99c61f14cc28abbedbbaee69ea5bfb759c06a
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-12-19 20:56:23 +0300

    feat: add task Exes and Ohs

--- 7kyu/Exes and Ohs.js
+++ 7kyu/Exes and Ohs.js
@@ -13,3 +13,11 @@ function XO(str) {
 
     return countX === countO;
 }
+
+
+
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+}
